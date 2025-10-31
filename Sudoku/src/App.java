7e2652c6c20867467c32319fe5e99dee79f20efc
vikranthvicky0116-crypto <vikranth.vicky0116@gmail.com>
2025-10-31 19:52:15 +0530

    Add files via upload

---
+++ Sudoku/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Sudoku sudoku = new Sudoku();
+        //sudoku.generatePuzzle(9); // Generate a 9x9 Sudoku puzzle
+    }
+}
